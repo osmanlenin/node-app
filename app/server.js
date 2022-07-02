c3b4330dc25b15dac492a3dc4616b6d0e6baabc6
osmanlenin <olgsevilla@gmail.com>
2022-07-02 18:08:54 -0500

    Update server.js

--- app/server.js
+++ app/server.js
@@ -8,14 +8,8 @@ const logger = pino({
     timestamp: () => `,"time":"${new Date().toISOString()}"`
 });
 
-logger.info('hello elastic world');
-logger.info('This is some great stuff');
-logger.info('Some more entries for our logging');
-logger.info('another line');
-logger.info('This never stops');
-logger.info('Logging logging all the way');
-logger.info('I think this is enough');
-logger.info('nope, one more!');
+logger.info('Hello Kubernnetes world');
+
 
 app.listen(3000, function () {
     logger.info("app listening on port 3000!");
